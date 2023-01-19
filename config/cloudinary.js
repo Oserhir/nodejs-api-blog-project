36a5ebd2714ac409f43cbee5ba0f36983c010dd9
OSERHIRDEV <oserhir10@gmail.com>
2023-01-19 12:53:22 +0100

    update

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,15 +2,14 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-//configure cloudinary
-
+// @desc configure cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 });
 
-//Instance of cloudinary storage
+// @desc Instance of cloudinary storage
 const storage = new CloudinaryStorage({
   cloudinary,
   allowedFormats: ["jpg", "png"],
